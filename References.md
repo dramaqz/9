057ac1eaa927e734af73dc4c047755c2db54c5b7
zenthebeagle <58642746+zenthebeagle@users.noreply.github.com>
2023-12-08 10:19:54 -0800

    Add files via upload

---
+++ References.md
@@ -0,0 +1,8 @@
+### References
+
+* [Hex Color Clock](https://github.com/saadq/Hex-Color-Clock)
+* quote
+  * https://freshman.tech/random-quote-machine/
+  * https://github.com/lukePeavey/quotable
+* random words
+  * https://matthewbibby.com/random/
---
+++ js/hexclock.js
@@ -0,0 +1,96 @@
+(function() {
+  'use strict';
+
+  const endpoint = 'https://api.quotable.io/random'
+  //'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
+
+  function getQuote() {
+  fetch(endpoint)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+        // data.message
+      displayQuote( `“${data.content}” \n -- ${data.author}`);
+    })
+    .catch(function () {
+      console.log("An error occurred");
+    });
+}
+
+function getSentence() {
+
+var phrases = [
+"I have been thinking about you for",
+//"I have been missing you for",
+"We have known each other for",
+"We have been together for",
+"You have had 'dramas' for",
+"I have been falling for you for",
+"You are the sunshine of my life for",
+"Since my love for you grows stronger it's been",
+"I've cherished every moment with you for",
+"Our love story has blossomed over",
+"I've been dreaming of you for",
+"Our love has stood the test of time for",
+"You are the melody in the soundtrack of my life for",
+];
+return phrases[Math.floor(Math.random() * phrases.length)];
+}
+
+function displayQuote(quote) {
+  const quoteText = document.querySelector('.quote-text');
+  quoteText.textContent = quote;
+}
+
+  function hexClock() {
+    // var clock = new Date();
+
+    var now = new Date();
+    var theDate = new Date('June 1, 2023 22:59:00');
+
+    // get total seconds between the times
+    var delta = Math.abs(now - theDate) / 1000;
+
+    // calculate (and subtract) whole days
+    var days = Math.floor(delta / 86400);
+    delta -= days * 86400;
+
+    // calculate (and subtract) whole hours
+    var hours = Math.floor(delta / 3600) % 24;
+    delta -= hours * 3600;
+
+    // calculate (and subtract) whole minutes
+    var mins = Math.floor(delta / 60) % 60;
+    delta -= mins * 60;
+
+    // what's left is seconds
+    var secs = delta % 60;  // in theory the modulus is not required
+    secs = parseInt(secs);
+
+    // var hours = clock.getHours(),
+    //     mins = clock.getMinutes(),
+    //     secs = clock.getSeconds();
+
+    if(hours <= 9) hours = '0' + hours;
+    if(mins <= 9) mins = '0' + mins;
+    if(secs <= 9) secs = '0' + secs;
+
+
+    var time = parseInt(days) + ' days ' + hours + ' hours ' + mins + ' minutes ' + secs + ' seconds ';
+
+
+    var color = '#' + hours + mins + secs;
+    document.body.style.background = color;
+    document.getElementById('time').innerHTML = time;
+    document.getElementById('hexColor').innerHTML = color;
+
+
+    setTimeout(hexClock, 1000);
+  }
+
+  hexClock();
+
+  getQuote();
+  document.getElementById('sentence').innerHTML = getSentence();
+}());
